2f179d63e5ab52d3653ec5db79874dd1f8d06e5c
kevinchacko <kevinpc1997@gmail.com>
2022-05-16 19:32:06 +0530

    debug true trial heroku

--- flask_app.py
+++ flask_app.py
@@ -16,8 +16,8 @@ import os
 app = Flask(__name__)
 
 load_dotenv(".env")
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
